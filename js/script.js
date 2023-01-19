5e2786113c06456c4e2af8f7b4aeccadfa023547
vraiSlophil <slophil.contact@gmail.Com>
2023-01-19 15:39:50 +0100

    LAST COMMIT

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+const menuButton = document.querySelector("#header__menu-button");
+
+menuButton.addEventListener("click", toggleMenu);
+
 function toggleMenu(){
-    var menu = document.getElementById("header__menu");
-    var button = document.getElementById("header__menu-button");
+    let menu = document.getElementById("header__menu");
+    let button = document.getElementById("header__menu-button");
     
     if (menu.classList.contains("MenuHidden")){
         menu.classList.remove("MenuHidden");
@@ -18,13 +22,13 @@ function toggleMenu(){
 }
 
 function toggleList(element) {
-    if (element.classList.contains("MobileHidden")){
+    if (element.classList.contains("MobileHidden") && window.innerWidth <= 870){
         element.classList.remove("MobileHidden");
         element.classList.add("MobileShow");
         element.style.display = "flex";
         return;
     }
-    if (element.classList.contains("MobileShow")) {
+    if (element.classList.contains("MobileShow") && window.innerWidth <= 870) {
         element.classList.remove("MobileShow");
         element.classList.add("MobileHidden");
         element.style.display = "none";
@@ -34,7 +38,7 @@ function toggleList(element) {
 }
 
 function slideUpdater(){
-    let slides = [document.getElementById("main__slide__first"),
+    const slides = [document.getElementById("main__slide__first"),
                     document.getElementById("main__slide__second"),
                     document.getElementById("main__slide__third"),
                     document.getElementById("main__slide__fourth"),
@@ -43,12 +47,23 @@ function slideUpdater(){
                     document.getElementById("main__slide__seventh"),
                     document.getElementById("main__slide__eighth"),
                     document.getElementById("main__slide__ninth")];
-    for (i = 0; i < 9; i++) {
-        if (slides[i].classList.contains("ActiveSlide")){
-            return;
-        }
-        return;
-    }
 
-    
+    let index = 0;
+    slides.forEach((element) => {
+        if (element.classList.contains("ActiveSlide")) {
+            element.classList.remove("ActiveSlide");
+            element.classList.add("HiddenSlide");
+            if (index === 8) {
+                slides[0].classList.remove("HiddenSlide");
+                slides[0].classList.add("ActiveSlide");
+                return
+            }
+            slides[index + 1].classList.remove("HiddenSlide");
+            slides[index + 1].classList.add("ActiveSlide");
+            return
+        }
+        index++;
+    });
 }
+
+setInterval(slideUpdater, 10000)
